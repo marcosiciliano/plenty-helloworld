ad0d8f880c56b5958847b3fb152451eb99d6e0b9
Marco Siciliano <info@sevenapp.de>
2016-11-18 14:18:19 +0100

    Initial checkin.

---
+++ src/Controllers/ContentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace plenty-helloworld\Controllers;
+
+
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Templates\Twig;
+
+class ContentController extends Controller
+{
+    public function sayHello(Twig $twig):string
+    {
+        return $twig->render('plenty-helloworld::content.hello');
+    }
+}
